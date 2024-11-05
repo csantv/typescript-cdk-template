6b62de20922e273885ae7089f92e4023737c7877
csantv <csantve@gmail.com>
2024-11-05 15:04:11 -0500

    update versions

--- lib/cdk-stack.ts
+++ lib/cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import {aws_s3 as s3} from "aws-cdk-lib";
 import {Construct} from "constructs";
+import { NagSuppressions } from "cdk-nag";
 
 export class CdkStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -10,8 +11,13 @@ export class CdkStack extends cdk.Stack {
         const environment = process.env.ENVIRONMENT ?? "dev";
         const namePrefix = `${appName}-${environment}`;
 
-        new s3.Bucket(this, `${namePrefix}-bucket`, {
+        const bucket = new s3.Bucket(this, `${namePrefix}-bucket`, {
             enforceSSL: true
         });
+
+        // add an exception to cdk-nag
+        NagSuppressions.addResourceSuppressions(bucket, [
+            {id: "AwsSolutions-S1", reason: "Access Logs Not Needed"}
+        ]);
     }
 }
